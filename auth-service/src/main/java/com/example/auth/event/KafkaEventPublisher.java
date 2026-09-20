64d350bbe2815c674791f65314c05f0c1a355f27
DecentSahil <168282363+DecentSahil@users.noreply.github.com>
2026-09-20 14:27:56 +0530

    Added asynchronous communication using kafka to send user id email and role to user-service

--- auth-service/src/main/java/com/example/auth/event/KafkaEventPublisher.java
+++ auth-service/src/main/java/com/example/auth/event/KafkaEventPublisher.java
@@ -2,6 +2,7 @@ package com.example.auth.event;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -9,23 +10,19 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaEventPublisher implements EventPublisher {
 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
     @Override
     public <T> void publish(
             String topic,
             String key,
             EventEnvelope<T> event
     ) {
-        log.info(
-                "Event published | topic={} | key={} | eventId={} | eventType={} | eventVersion={} | occurredAt={} | payload={}",
-                topic,
-                key,
-                event.eventId(),
-                event.eventType(),
-                event.eventVersion(),
-                event.occurredAt(),
-                event.payload()
-        );
+        log.info("Publishing event to Kafka | topic={} | key={} | eventId={}", topic, key, event.eventId());
+        try {
+            kafkaTemplate.send(topic, key, event);
+        } catch (Exception ex) {
+            log.error("Failed to publish event to topic {}: {}", topic, ex.getMessage(), ex);
+        }
     }
-
-
 }
